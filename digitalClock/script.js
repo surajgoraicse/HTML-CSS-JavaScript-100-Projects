d31b68a3e23b5b6983fdd7861c9cf9d8ca61e835
surajgoraicse <surajgoraicse@gmail.com>
2024-06-24 09:40:29 +0530

    digital clock deleted

--- digitalClock/script.js
+++
@@ -1,9 +0,0 @@
-// console.log(time.toLocaleTimeString());
-
-function showTime() {
-    const time = new Date()
-    document.querySelector('.clock').innerHTML = time.toLocaleTimeString()
-}
-
-
-setInterval(showTime, 1000);
